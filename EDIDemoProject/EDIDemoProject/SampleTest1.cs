a6e8311169c6a58a9beafffa9c708a9acc2097c7
sreddiar <sarguna.reddiar@gmail.com>
2016-07-06 12:42:06 -0600

    base commit

---
+++ EDIDemoProject/EDIDemoProject/SampleTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace EDIDemoProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(BaseLibrary.FileOperation.CopyFile(@"C:\Users\chuck\Desktop\Projects\CaptureRX\source",
+                        @"C:\Users\chuck\Desktop\Projects\CaptureRX\target",
+                        "test.txt"));
+        }
+
+
+
+    }
+}
---
+++ EDIDemoProject/EDIDemoProject/SampleTest2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace EDIDemoProject
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            Assert.IsTrue(BaseLibrary.FileOperation.CopyFile(@"C:\Users\chuck\Desktop\Projects\CaptureRX\source",
+                        @"C:\Users\chuck\Desktop\Projects\CaptureRX\target",
+                        "test.txt"));
+        }
+
+
+
+    }
+}
