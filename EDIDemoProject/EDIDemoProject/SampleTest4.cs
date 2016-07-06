1a3ac1d87abbba0e422336cba6f404ce08780dd9
sreddiar <sarguna.reddiar@gmail.com>
2016-07-06 15:09:49 -0600

    update

--- EDIDemoProject/EDIDemoProject/SampleTest4.cs
+++ EDIDemoProject/EDIDemoProject/SampleTest4.cs
@@ -10,7 +10,7 @@ namespace EDIDemoProject
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(BaseLibrary.FileOperation.CopyFile(@"C:\Users\chuck\Desktop\Projects\CaptureRX\source",
+            Assert.IsFalse(BaseLibrary.FileOperation.CopyFile(@"C:\Users\chuck\Desktop\Projects\CaptureRX\source",
                         @"C:\Users\chuck\Desktop\Projects\CaptureRX\target",
                         "test.txt"));
         }
